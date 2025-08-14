57a6e81b6257ea1659d0232b33685ff3c05fc481
Sergey <86160616+yoonost@users.noreply.github.com>
2025-08-15 02:31:37 +0300

    Add client interface and CI workflows
    Introduces a new ConfigClientInterface in lib/interfaces/client.interface.ts and exports it in lib/index.ts. Adds GitHub Actions workflows for testing and publishing. Also updates server.controller.ts to clean up imports.

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,10 @@
 export * from './interfaces/server.interface'
+export * from './interfaces/client.interface'
 
 export * from './server/server.controller'
 export * from './server/server.module'
 export * from './server/server.service'
+
+// export * from './client/client.controller'
+// export * from './client/client.module'
+// export * from './client/client.service'
---
+++ lib/interfaces/client.interface.ts
@@ -0,0 +1,5 @@
+export interface ConfigClientInterface {
+    hostname: string
+    environment: string
+    serviceName: string
+}
